342e5a30e7a5ac1900d91b867542d089d9939452
Thomas Müller <muelletm@gmail.com>
2023-05-27 19:17:46 +0000

    Small fix.

--- run.sh
+++ run.sh
@@ -10,5 +10,4 @@ docker build . -t qlora
 mkdir -p output
 docker run --rm --name qlora --gpus all \
   --mount type=bind,source="$PWD/output",target=/output \
-  --mount type=bind,source="$PWD/mpt-7b",target=/mpt-7b \
-  qlora train_mpt.sh
+  qlora train.sh
